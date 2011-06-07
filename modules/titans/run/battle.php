a7e7a7f53c9a19b4bee25660466bb5fc64048250
Beeps <HgDelirium@gmail.com>
2011-06-07 00:59:42 -0400

    A hopeful fix for keeping players from getting bumped to IC when they die on a Titan.

--- modules/titans/run/battle.php
+++ modules/titans/run/battle.php
@@ -23,9 +23,9 @@ if ($battle){
 		titans_show_comrades($titan);
 		titans_leave_battle_ko($titan);
 		require_once("lib/forestoutcomes.php");
-		forestdefeat(array($badguy),", a Titan");
 		//set the player's Location properly so they don't get bumped back to IC
 		$session['user']['location'] = get_module_pref("lastCity","worldmapen");
+		forestdefeat(array($badguy),", a Titan");		
 	} else {
 		require_once("lib/fightnav.php");
 		fightnav(true,true,"runmodule.php?module=titans&titanop=battle&titanid=".$titanid, true);
