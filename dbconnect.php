9f4405a9890b0afeec1347077ba6dda2beaf1c72
Cousjava <outingbrains-websitesgeneric@yahoo.co.uk>
2012-02-20 18:19:08 +0000

    added a copy of turretdefense

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+//This file automatically created by installer.php on Apr 25, 2008 04:30 pm
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASS = 'password';
+$DB_NAME = 'dragonprime';
+$DB_PREFIX = 'free_';
+$DB_USEDATACACHE = 0;
+$DB_DATACACHEPATH = 'datacache';
+?>
